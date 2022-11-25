7f33f46a5219ed8b14a68317803dcac00c36fa3d
kiran sonne <kiransonne04@gmail.com>
2022-11-25 10:18:29 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kiran Sonne on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
